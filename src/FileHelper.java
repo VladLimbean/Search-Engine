ef24c4f97a2ac4885457fbdf1bffb8dacbbd9fe2
imlad <ivanmladenov91@gmail.com>
2016-10-04 00:45:09 +0200

    Small refactoring and more comments added
    Also the missing message "Entering "quit" will stop the program" that
    should show up to the user based on the hand-in requirements

--- src/FileHelper.java
+++ src/FileHelper.java
@@ -13,36 +13,52 @@ public class FileHelper {
         String url = null;
         String title = null;
         List<String> wordList = null;
+
         try {
             Scanner sc = new Scanner(new File(arg));
             while (sc.hasNext()) {
                 String line = sc.next().trim();
                 if (line.startsWith("*PAGE:")) {
-                    // new entry starts
-                    // save the old one
-                    // if checks if url, title and wordList are set
-                    if ((url != null)&&(title!=null)&&(wordList!=null)) {
+                    //new entry starts
+                    //save the old one
+                    //do not add the website if url, title and wordList are not set
+                    if ((url != null) && (title!=null) && (wordList!=null)) {
                         result.add(new Website(url, title, wordList));
                     }
+
+                    //Reset the title and word list
                     title = null;
                     wordList = null;
+                    //Set the url to be equal to this line - the *PAGE: prefix
                     url = line.substring(6);
-                } else if (url != null && title == null) {
+                }
+                else if (url != null && title == null) {
+                    //This code enters when the line following *PAGE: is read
+                    //This means this is the title
                     title = line;
-                } else if (url != null && title != null) {
+                }
+                else if (url != null && title != null) {
+                    //This code enters when both url and title are set
+                    //This means that this is every line that is not the 1st or 2nd one
+
+                    //Create the list if it is null, so no errors happen
                     if (wordList == null) {
                         wordList = new ArrayList<String>();
                     }
+                    //Add the word to the list of words
                     wordList.add(line);
                 }
             }
-            // if checks if url, title and wordList are set
-            if ((url != null)&&(title!=null)&&(wordList!=null)) {
+            //Loop exits
+            //Add the last website, but only if url, title and word list are set
+            if ((url != null) && (title!=null) && (wordList!=null)) {
                 result.add(new Website(url, title, wordList));
             }
-        } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
         return result;
     }
 }
