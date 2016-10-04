d88fe141fedd4cd1588124558c64c22f4575baec
Vlad Limbean <vlli@itu.dk>
2016-10-04 13:24:41 +0200

    Index interface + class implementation SimpleIndex

---
+++ src/Index.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Vlad on 04/10/2016.
+ */
+public interface Index {
+
+    void build();
+
+    String lookup();
+
+}
---
+++ src/SimpleIndex.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Vlad on 04/10/2016.
+ */
+public class SimpleIndex implements Index {
+    @Override
+    public void build() {
+
+    }
+
+    @Override
+    public String lookup() {
+        return null;
+    }
+}
