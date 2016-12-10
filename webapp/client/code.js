42729c4c3e990de4cbd85ecddaed7633ac11c321
ivml <ivml@itu.dk>
2016-12-10 16:31:12 +0100

    Javascript code now cheks for valid input

--- webapp/client/code.js
+++ webapp/client/code.js
@@ -2,15 +2,27 @@ $(document).ready(function() {
     var baseUrl = "http://localhost:8080";
 
     $("#searchbutton").click(function() {
+		//Check input for correctness
+		var input = $('#searchbox').val();
+		if (input == "") {
+			$("#responsesize").html("<p>Input is empty</p>");
+			return;
+		}
+		
         console.log("Sending request to server.");
         $.ajax({
             method: "GET",
             url: baseUrl + "/search",
-            data: {query: $('#searchbox').val()}
+            data: {query: input}
         }).success( function (data) { 
             console.log("Received response " + data);
 			
             $("#responsesize").html("<p>" + data.websites.length + " results found in " + data.time + " ms</p>");
+			
+			if (data.websites.length == 0) {
+				return;
+			}
+			
             buffer = "<div class=\"results-container\">";
             $.each(data.websites, function(index, value) {
 				buffer += "<div class=\"result-entry\">";
